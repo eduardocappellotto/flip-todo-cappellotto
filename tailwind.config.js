a726574362468707ad7bc80b03a0654bd3e6ec34
Eduardo <eduardocappellotto@gmail.com>
2023-08-19 22:47:19 -0300

    feat: tailwindcss base config (adicionado cores do site da flip saúde)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary-flip': '#53b8f4',
+        'secondary-flip': '#97ff00'
+      }
+    },
   },
   plugins: [],
 }
